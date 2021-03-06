6678ca0b863a1b927f3f919f06319714ad8c6107
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2021-03-06 20:31:04 -0300

    Iniciando o frontend

---
+++ web/src/index.jsx
@@ -0,0 +1,5 @@
+import react from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ web/src/main/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react' 
+
+const App = function () {
+    <div className="wrapper">
+        <h1>App</h1>
+    </div>
+}
